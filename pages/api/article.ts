90ac09489ea938527657543434bd126442325f2f
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2022-12-24 12:22:54 +0800

    Update /article endpoint to return article content from url

--- pages/api/article.ts
+++ pages/api/article.ts
@@ -1,16 +1,38 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+import axios, { AxiosError } from 'axios';
+import sites from '../../data/sites-supported';
+const cheerio = require('cheerio');
 
 type Data = {
   url: string;
   content: string;
 };
 
-export default function handler(req: NextApiRequest, res: NextApiResponse<Data | string>) {
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data | string>) {
   if (req.body.url === undefined) {
     res.status(400).send('Missing input url');
   }
 
   const url = req.body.url;
+  const articleSite = new URL(url).hostname;
 
-  res.status(200).json({ url, content: '' });
+  const hostnamesSupported = sites.map((site) => site.hostname);
+  const currentSite = sites.find((site) => site.hostname === articleSite);
+
+  // Return an error is the site isn't supported
+  if (!hostnamesSupported.includes(articleSite)) {
+    res
+      .status(400)
+      .send(`Unsupported site. Supported sites include: ${hostnamesSupported.join(',')}`);
+  }
+
+  // Retrieve article body/content
+  const body = await axios.get(url, {
+    headers: { 'Accept-Encoding': 'gzip,deflate,compress' },
+  });
+
+  const $ = cheerio.load(body.data);
+  const articleContent = $(currentSite?.query).text();
+
+  res.status(200).json({ url, content: articleContent });
 }
