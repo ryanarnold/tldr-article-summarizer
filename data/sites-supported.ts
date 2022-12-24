4bbbb628cfb65837bf3233090bbf555fc074bf7b
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2022-12-24 13:45:12 +0800

    feat/basic ui

--- data/sites-supported.ts
+++ data/sites-supported.ts
@@ -1,10 +1,14 @@
 const sites = [
   {
+    name: 'The Guardian',
     hostname: 'www.theguardian.com',
+    fullurl: 'https://www.theguardian.com/international',
     query: '#maincontent p',
   },
   {
+    name: 'The Verge',
     hostname: 'www.theverge.com',
+    fullurl: 'https://www.theverge.com/',
     query: 'article p',
   },
 ];
