cb2dfa5928062f61acd4838825cfe3391c8134e2
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2022-12-24 12:22:27 +0800

    Add initial sites supported data

---
+++ data/sites-supported.ts
@@ -0,0 +1,12 @@
+const sites = [
+  {
+    hostname: 'www.theguardian.com',
+    query: '#maincontent p',
+  },
+  {
+    hostname: 'www.theverge.com',
+    query: 'article p',
+  },
+];
+
+export default sites;
